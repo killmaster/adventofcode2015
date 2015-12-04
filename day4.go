c5e2f2cc85e7a4ab411be9b56d5e7dcc18a9f154
Carlos Martins <carlosmartins8@gmail.com>
2015-12-04 06:59:38 +0000

    clean up

--- day4.go
+++ day4.go
@@ -9,19 +9,16 @@ import (
 var key = "yzbqklnj"
 
 func main() {
-	count := 1
-	countString := strconv.Itoa(count)
-	keyAnswer := key + countString
-	hash := md5.Sum([]byte(keyAnswer))
-	answer := fmt.Sprintf("%x", hash)
-
-	for answer[0:6] != "000000" {
+	count := 0
+	for {
 		count++
 		countString := strconv.Itoa(count)
-		keyAnswer = key + countString
-		hash = md5.Sum([]byte(keyAnswer))
-		answer = fmt.Sprintf("%x", hash)
+		keyAnswer := key + countString
+		hash := md5.Sum([]byte(keyAnswer))
+		answer := fmt.Sprintf("%x", hash)
+		if answer[0:6] == "000000" {
+			break
+		}
 	}
-
 	fmt.Println("Result: ", count)
 }
