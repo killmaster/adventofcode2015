ae4cf687a5ba2154495936633de445d49c12c177
Carlos Martins <carlosmartins8@gmail.com>
2015-12-04 20:03:49 +0000

    threaded solution

---
+++ day4threaded.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strconv"
+)
+
+var key = "yzbqklnj"
+var result = make(chan string)
+
+const max = 10000000
+
+func worker(id int, jobs <-chan int, result chan<- string) {
+	for answer := range jobs {
+		//fmt.Println("worker ", id, "hashing ", answer)
+		answerString := strconv.Itoa(answer)
+		keyAnswer := key + answerString
+		hash := md5.Sum([]byte(keyAnswer))
+		hashString := fmt.Sprintf("%x", hash)
+		if hashString[0:6] == "000000" {
+			result <- answerString
+			break
+		}
+	}
+}
+
+func main() {
+	jobs := make(chan int, max)
+	result := make(chan string)
+
+	for w := 0; w <= 10; w++ {
+		go worker(w, jobs, result)
+	}
+
+	for j := 1; j <= max; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	select {
+	case msg1 := <-result:
+		fmt.Println("Result: ", msg1)
+	}
+}
