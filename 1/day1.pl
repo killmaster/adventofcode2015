14eaa9722cc7eea7d41fdae4627fe4c93b643633
Carlos Martins <carlosmartins8@gmail.com>
2015-12-07 18:56:50 +0000

    perl added

---
+++ 1/day1.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Path::Class;
+use autodie;
+
+my $file = file("day1input.txt");
+
+my $file_contents = $file->slurp();
+
+my $count_up = () = $file_contents =~ m/\(/g;
+my $count_down = () = $file_contents =~ m/\)/g;
+
+my $floor = $count_up - $count_down;
+
+my $current = 0;
+my $count = 0;
+while ($file_contents =~ m/([\(\)])/g) {
+  my $char = $1;
+  if($char =~ m/\(/) {
+    $current++;
+  } else {
+    $current--;
+  }
+  $count++;
+  if($current < 0) {
+    last;
+  }
+}
+print "open: $count_up\tclosed: $count_down\tfloor: $floor\n";
+print "part 2: $count\n";
